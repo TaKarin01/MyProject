8fb3da1bbeb11cb26aa780f9d9121c87ac4a42c3
TaKarin01 <mailam493010@gmail.com>
2024-07-11 12:57:32 +0700

    Commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\Users\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/admin/users/login',[LoginController::class,'index'])->name('login');
